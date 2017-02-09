9f25bb8b29b79d8fa6dace5d4b65a291741d09ea
Zjianglin <596614217@qq.com>
2017-02-09 15:27:46 +0800

    parse string

--- source/leptjson.h
+++ source/leptjson.h
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <string>
+#include <memory>
 #include <cassert>
 
 enum lept_type
@@ -20,6 +21,10 @@ struct lept_value
 {
     union  {
         double num;
+        struct { 
+            std::shared_ptr<char*> s; 
+            sizt_t len; 
+        }s;
     } u;
     lept_type type;
 };
@@ -40,13 +45,27 @@ class LeptJson
 
     int parse(const std::string &json) ;
 
-    void set_type(const lept_type nt) { parsed_v_.type = nt; }
-    lept_type   get_type() const { return parsed_v_.type; }
-    double      get_number() const { assert(parsed_v_.type == LEPT_NUMBER); return parsed_v_.u.num; }
+    void set_type(const lept_type nt)   { parsed_v_.type = nt; }
+    void set_null()                     { parsed_v_.type = LEPT_NULL;}
+    void set_boolean(unsigned char b)   { parsed_v_.type = ( b ? LEPT_TRUE : LEPT_FALSE) ;}
+    void set_number(double n)           { parsed_v_.type = LEPT_NUMBER; parsed_v_.u.num = n; }
+    void set_string(const char *s, size_t len);
+    
+    lept_type   get_type() const        { return parsed_v_.type; }
+    double      get_number() const      { assert(parsed_v_.type == LEPT_NUMBER); return parsed_v_.u.num; }
+    int         get_boolean() const     
+    { assert(parsed_v_.type == LEPT_FALSE || parsed_v_.type == LEPT_TRUE); return parsed_v_.type; }
+    const char* get_string() const;
+    sizt_t      get_string_length() const;
 
   private:
     struct lept_context {
         const char *json;
+        std::shared_ptr<char*> stack;
+        size_t size, top;
+
+        std::shared_ptr<void*> push(size_t count);
+        std::shared_ptr<void*> pop(size_t count);
     };
     lept_value parsed_v_;
 
